80748cf39c82196dee2802e71da7a4626f227674
Benson <AKLbc70@gmail.com>
2014-10-19 10:01:06 +1300

    Update AndroidBoard.mk

--- hwy2100100/AndroidBoard.mk
+++ hwy2100100/AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/huawei/hwy2100100/AndroidBoardVendor.mk
+-include vendor/huawei/U8685d/AndroidBoardVendor.mk
