07c4c3d282e5c633acf6874aaa7ca4eb2a6b802f
Benson <AKLbc70@gmail.com>
2014-10-19 10:02:16 +1300

    Update BoardConfig.mk

--- hwy2100100/BoardConfig.mk
+++ hwy2100100/BoardConfig.mk
@@ -1,12 +1,12 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/huawei/hwy2100100/BoardConfigVendor.mk
+-include vendor/huawei/U8685d/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := unknown
 TARGET_CPU_ABI := armeabi
-TARGET_BOOTLOADER_BOARD_NAME := hwy2100100
+TARGET_BOOTLOADER_BOARD_NAME := U8685d
 
 BOARD_KERNEL_CMDLINE := console=ttyDCC0 androidboot.hardware=huawei
 BOARD_KERNEL_BASE := 0x00200000
@@ -19,8 +19,8 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 195035136
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 167772160
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-TARGET_PREBUILT_KERNEL := device/huawei/hwy2100100/kernel
+TARGET_PREBUILT_KERNEL := device/huawei/U8685d/kernel
 
 #BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
-#BOARD_HAS_LARGE_FILESYSTEM : true
+#BOARD_HAS_LARGE_FILESYSTEM : false
